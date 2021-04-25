6400f589daadda690a3ac2cf7611d8ac10eff292
martina contestabile <martina.contestabile01@gmail.com>
2021-04-25 13:02:35 +0200

    Cosine varie

--- Codice-Fiscale/Codice-Fiscale/src/it/unibs/fp/codice_fiscale/CodiceFiscale.java
+++ Codice-Fiscale/Codice-Fiscale/src/it/unibs/fp/codice_fiscale/CodiceFiscale.java
@@ -1,7 +1,8 @@
 package it.unibs.fp.codice_fiscale;
 
+//@Author Baresi Marco, Contestabile Martina, Iannella Simone
 public class CodiceFiscale {
-    public static void main (String args []) {
+    public static void main (String[] args) {
 
     }
 }
